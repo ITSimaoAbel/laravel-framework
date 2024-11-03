8fcae0f0ddc9ae5d29084c78730af47420ec7f23
ITSimaoAbel <simao3cuco@gmail.com>
2024-11-03 14:58:38 +0200

    Knowing blade

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $nome = "Simao";
+    $idade = 25;
+
+    return view('welcome', ['nome' => $nome, 'idade' => $idade, 'profissao' => "Programador"]);
 });
 
 Route::get('/contact', function () {
