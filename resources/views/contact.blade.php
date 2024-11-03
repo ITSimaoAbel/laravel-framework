9ca8cf88d8a5b64a00480aa9a88780888f4bc91d
ITSimaoAbel <simao3cuco@gmail.com>
2024-11-03 13:13:20 +0200

    Creating Routes

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,2 +1,3 @@
 <h1>Esta e a pagina de contacto</h1>
 <a href="/">Voltar para home</a>
+<p>Esta view foi feita para testar as rotas</p>
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,1 +1,3 @@
 <h1>Pagina de productos</h1>
+<p>Esta view foi feita para testar as rotas</p>
+<a href="/">Voltar para home</a>
