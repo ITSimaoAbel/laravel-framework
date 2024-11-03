5f4ea9a3b85fc6cc908217a3e9e94600474e9815
ITSimaoAbel <simao3cuco@gmail.com>
2024-11-03 16:48:55 +0200

    Adicionando arquivo main.blade.php

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link rel="stylesheet" href="/css/styles.css">
+        <script src="/js/script.js"></script>
+
+    </head>
+    <body>
+        <h1>Hello Blade</h1>
+        <img src="/img/imagem1.png" alt="Alguma imagem">
+        @if(10 > 5)
+            <p>A condicao e true</p>
+        @endif
+
+        <p>{{ $nome }}</p>
+
+        @if($nome == "Pedro")
+        <p>O nome e Pedro</p>
+        @elseif($nome == "Simao")
+        <p>O nome e {{ $nome }} e ele tem {{ $idade }} anos, e ele trabalha como {{ $profissao }}</p>
+        @else
+        <p>O nome nao e Pedro</p>
+        @endif
+
+        @for($i = 0; $i < count($arr); $i++)
+            <p>{{ $arr[$i] }} - {{ $i }}</p>
+            @if($i == 2)
+            <p>O i e 2</p>
+            @endif
+        @endfor
+
+        @foreach($nomes as $nome)
+            <p>{{ $loop->index }}</p>
+            <p>{{ $nome }}</p>
+        @endforeach
+
+        @php
+            $name = "Maria";
+            echo $name; 
+        @endphp
+
+        <!-- Comentario html - nao aconselhado -->
+         {{-- Comentario com blade --}}
+    </body>
+</html>
