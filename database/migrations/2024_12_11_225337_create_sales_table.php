dc4d4c73f558ab6f97ccbb6ec29b5cc437ea833d
ITSimaoAbel <simao3cuco@gmail.com>
2024-12-12 01:06:18 +0200

    Create migrations

---
+++ database/migrations/2024_12_11_225337_create_sales_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->foreignID('user_id')->constrained()->onDelete('cascade');
+            $table->decimal('total_amount', 10, 2);
+            $table->timestamp('sale_date')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
